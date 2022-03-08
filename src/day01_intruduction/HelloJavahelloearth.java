c967a2f552f66d5c3c7791e25c1559e79c6ca6f7
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-03-08 15:40:51 -0500

    first commit

---
+++ src/day01_intruduction/HelloJavahelloearth.java
@@ -0,0 +1,6 @@
+package day01_intruduction;
+
+public class HelloJavahelloearth {
+}
+
+
---
+++ src/day01_intruduction/HelloWorld.java
@@ -0,0 +1,20 @@
+package day01_intruduction;
+
+public class HelloWorld {
+
+    public static void main(String[] args ) {
+
+        System.out.println("Hello World");
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
