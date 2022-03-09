71667b73c3f46643f513e462b6272a48f3357f9b
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-03-08 20:42:04 -0500

    update

--- src/day04_Operators/c2_task.java
+++ src/day04_Operators/c2_task.java
@@ -9,7 +9,7 @@ public class c2_task {
         //ex : x=25  , y= 32 , z= 40 .
         //ex : x=20 , y= 27 , z= 35 .
 
-        int x= 20; // 20
+        int x= 30; // 20
         //y =17    // 27
         //z =25    // 35
 
@@ -22,6 +22,29 @@ public class c2_task {
 
         //sum of those 3 numbers
         System.out.println("Sum of numbers are : " + (x+y+z) );
+
+        //if x is divisible by 2,3, 5
+        //if the x is divisble by 2 ,3 ,5
+//        3. check if x is divisible by 2 , 3 , 5  (Check for each number)
+//                      if it is divisible print : x is divisible by 2  true
+//                      if it is not divisible print : x is divisible by 2 false
+
+        //x=20
+        //this is divisible by2
+        //System.out.println("20 is divisible by 2 true");
+
+        //if number is divisible by 2 == that means number has remiander as 0
+        boolean divisibleBy2forX= x % 2 ==0; //if remiender is equal 0 that means number is divisible by 2
+
+        System.out.println(x + " is divisible by 2 :" + divisibleBy2forX);
+
+        boolean divisibleBy3forX= x % 3==0; //25%3 ==1 false
+
+        System.out.println(x + " is divisible by 3 : " + divisibleBy3forX);
+
+        System.out.println(35/3); //11
+        System.out.println(35%3); //2
+
 
     }
 }
---
+++ src/day04_Operators/c4_primitiveCasting.java
@@ -0,0 +1,14 @@
+package day04_Operators;
+
+public class c4_primitiveCasting {
+    //we have 2 type of primitive casting
+    //1.implicit casting (done automaticly from java)
+    //2.explicit casting (needs to be handle manually)
+
+    //double > float > long > int > short > byte
+    public static void main(String [] args){
+
+        byte number1=20; //-128 to 127
+        long number2=number1;
+    }
+}
