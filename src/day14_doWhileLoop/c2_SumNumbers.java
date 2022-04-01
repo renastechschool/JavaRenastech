78612766fe4617976c7c5db0681cd5cfde4da9d9
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-03-31 20:38:50 -0400

    practice

---
+++ src/day14_doWhileLoop/c2_SumNumbers.java
@@ -0,0 +1,20 @@
+package day14_doWhileLoop;
+
+public class c2_SumNumbers {
+
+    /// task
+    //Write a do-while loop that asks the user to enter two numbers.
+    // The numbers should be added and the sum displayed.
+    // The loop should ask the user whether he or she wishes to perform the operation again.
+    // If so, the loop should repeat; otherwise it should terminate.
+    // if user says y or Y continue
+    // if user says n or N stop the loop
+
+    public static void main(String[] args) {
+
+    }
+}
+
+
+//class color is red that means this class is not added to git
+//
