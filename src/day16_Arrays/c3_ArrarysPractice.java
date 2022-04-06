68914a79e7a824fc2e464a59e4dfc7ac12751ffd
mahiralkan <mahir.alkan.100@gmail.com>
2022-04-05 19:58:34 -0600

    Day16, Class3

--- src/day16_Arrays/c3_ArrarysPractice.java
+++ src/day16_Arrays/c3_ArrarysPractice.java
@@ -42,9 +42,42 @@ public class c3_ArrarysPractice {
         System.out.println(Arrays.toString(biggestToSmallest)); // [99, 90, 50, 34, -90]
 
 
+        System.out.println("*******************************************");
         ////write a program that can return the largest string of text from an array
         ////ex         String[] names = {"denver", "texas", "orlando", "Toronto", "jeniffer", "sevgin", "renastechschool"};
         ////output :renastechschool
+
+
+        //             len = 6        5        7          7          9      6                14
+        String[] names = {"denver", "texas", "orlando", "Toronto", "jeniffer", "sevgin", "renastechschool"}; // names lenght=7
+//                        0          1          2         3         4           5         6
+        //length of the index 2
+        System.out.println(names[2].length()); // 7
+
+        int maxLenghtIndex=names[0].length(); // assume that names index 0 has longest length;
+
+        for (int i = 0; i < names.length ; i++) {
+
+            System.out.println(""+ names[i]+ " has "+ names[i].length()+" lenght");
+            //   i=0    0< 7             i++
+            // i=1     1<7
+
+
+            //   names[0].lenght 6 > 6
+            //  name[1].lenght   5 > 6
+            if (names[i].length() > maxLenghtIndex){
+                maxLenghtIndex=i;
+
+            }
+
+        }
+
+        System.out.println("**********************");
+        System.out.println(names[maxLenghtIndex]);
+
+
+
+
 
 
 
