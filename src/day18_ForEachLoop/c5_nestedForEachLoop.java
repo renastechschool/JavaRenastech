8d389abdaa88016539574e2efe40cf884cc879da
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-04-08 21:41:29 -0400

    update

--- src/day18_ForEachLoop/c5_nestedForEachLoop.java
+++ src/day18_ForEachLoop/c5_nestedForEachLoop.java
@@ -11,7 +11,7 @@ public class c5_nestedForEachLoop {
 
         int [][] arr2D= { {1,2,3} , {4,5 } , {6,7,8,9,10,11,12} };
 
-        for (int [] each1D : arr2D) { //this will store each child container
+        for (int  []each1D : arr2D) { //this will store each child container
             //containers 0 1 2
 
             for (int eachData : each1D ){ //this will store each number from each container
