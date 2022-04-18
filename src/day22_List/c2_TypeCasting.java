25a009e514e1e3c5c667f53c972150e33f639a7f
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-04-18 19:47:55 -0400

    update

---
+++ src/day22_List/c2_TypeCasting.java
@@ -0,0 +1,26 @@
+package day22_List;
+
+public class c2_TypeCasting {
+
+    //Type Casting
+    //1.Autoboxing: Converting a primitive value into an object of the corresponding wrapper class is called autoboxing.
+    // For example, converting int to Integer class. The Java compiler applies autoboxing when a primitive value is:
+    //Passed as a parameter to a method that expects an object of the corresponding wrapper class.
+    //Assigned to a variable of the corresponding wrapper class.
+    //2.Unboxing: Converting an object of a wrapper type to its corresponding primitive value is called unboxing.
+    // For example conversion of Integer to int. The Java compiler applies unboxing when an object of a wrapper class is:
+    //Passed as a parameter to a method that expects a value of the corresponding primitive type.
+    //Assigned to a variable of the corresponding primitive type.
+    //The following table lists the primitive types and their corresponding wrapper classes,
+    // which are used by the Java compiler for autoboxing and unboxing:
+    //Primitive Data Type	Wrapper Class
+    //byte	                Byte
+    //short	                Short
+    //int	                Integer
+    //long	                Long
+    //float	                Float
+    //double	            Double
+    //boolean	            Boolean
+    //char	                Character
+
+}
