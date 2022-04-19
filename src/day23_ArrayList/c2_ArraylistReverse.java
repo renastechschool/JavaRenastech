9207f124ea4e8ec34bc8d6131dcb32cfcb124d67
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-04-19 19:29:09 -0400

    upd

---
+++ src/day23_ArrayList/c2_ArraylistReverse.java
@@ -0,0 +1,17 @@
+package day23_ArrayList;
+
+import java.util.ArrayList;
+
+public class c2_ArraylistReverse {
+    //   write a program that can print the list of integers in reversed order
+//    ex:
+//      list=> {1,2,3,4,5,6}
+//      output: 6 5 4 3 2 1
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list=new ArrayList<>();
+
+        //add  numbers from 1 to 10
+    }
+}
