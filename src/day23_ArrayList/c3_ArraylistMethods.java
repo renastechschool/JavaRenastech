b5ca26bff1ad222ff516b7ff51b72d69a47a2798
svgnhkmglu <sevginhekimoglu@gmail.com>
2022-04-19 20:27:08 -0400

    arraylist

--- src/day23_ArrayList/c3_ArraylistMethods.java
+++ src/day23_ArrayList/c3_ArraylistMethods.java
@@ -33,5 +33,53 @@ public class c3_ArraylistMethods {
         numbers.add(5,47);
         //numbers.add(47)
         System.out.println(numbers);//[20,5,100,50,500 ,47]
+
+        //set(index,data) //reassigning your datas
+        //method will set value for given index
+        //set 5 to 555 ; index number 5 wont be anymore 47 it will be 555
+        numbers.set(2,11);
+        System.out.println(numbers);//[20,5,11,50,500 ,47]
+
+        ArrayList<String > letters=new ArrayList<>();
+        System.out.println(letters);//[]
+
+        letters.add("B");//0
+        letters.add("X");//1
+
+        System.out.println(letters);//[B, X]
+
+        letters.add(1,"A");//[B, A,X]
+        System.out.println(letters);
+
+        letters.set(0,"N");
+        System.out.println(letters);//[N, A,X]
+
+        letters.add("A");
+        System.out.println(letters);//[N, A, X, A]
+
+        //remove
+        letters.remove("A");
+        System.out.println(letters);//[N, X, A]
+        letters.remove(0);
+        System.out.println(letters);//[X, A]
+
+        letters.remove("T");
+        System.out.println(letters);
+        ////remove method will look at given object or index to remove it if it is present it will removve
+        //        //if it is not presnet nothing will change
+
+        boolean b1=letters.remove("R"); //false
+        System.out.println(b1);
+
+        //clear
+        letters.clear();
+        System.out.println(letters);//[]
+
+        if (letters.size() == 0 ){
+            System.out.println("clear method worked");
+        }else {
+            System.out.println("Clear method is not working");
+        }
+
     }
 }
